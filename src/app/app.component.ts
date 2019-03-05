a9f5e5b03decc1cba3cdc84179fd1047d84e81e8
Djoda <victor.paco.flores@gmail.com>
2019-03-05 12:13:03 -0400

    assets agregados para la plantilla en general

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCurriculum';
-  login:boolean = false;
+  login:boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { LoginComponent } from './components/login/login.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
